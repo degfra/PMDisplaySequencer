76f73a63d459eed539268c272cc8bb05d2fe84e2
francois <francois@devstation>
2013-10-14 17:48:04 +0200

    Cleaned up clipslist.html.php

--- clips/cliplist.html.php
+++ clips/cliplist.html.php
@@ -16,18 +16,8 @@
                 <?php echo htmlout($clip['clipname'], ENT_QUOTES, 'UTF-8'); ?>
 
                  (submitted on : <?php echo htmlout($clip['clipdate'], ENT_QUOTES, 'UTF-8'); ?> ) 
-                 <!--
-                <?php echo $clip['headercode']; ?>
-                <?php echo $clip['clipcode']; ?>
-                <?php echo $clip['footercode']; ?>
-                 -->
-                
                 <input type="hidden" name="clip_id" value="<?php echo $clip['id']; ?>">
-                <!--
-                <input type="hidden" name="headercode" value="<?php echo $clip['headercode']; ?>">
-                <input type="hidden" name="clipcode" value="<?php echo $clip['clipcode']; ?>"> 
-                <input type="hidden" name="footercode" value="<?php echo $clip['footercode']; ?>"> 
-                -->
+
                 <input type="hidden" name="action" value="Preview" >
                 <input type="submit" value="Preview">
                 <!-- <input type="submit" name="action" value="Edit"> -->
